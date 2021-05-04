5d74c5fdd979ae7ebc6869f6c067cfdc1dccf1e5
lehung1212 <leviethung1212@gmail.com>
2021-05-04 12:22:10 +0700

    filter update

--- flask_server.py
+++ flask_server.py
@@ -70,14 +70,9 @@ def webhook():
             detectedLabel = detect_labels_uri(snapResponse['url'])
             print("Snapshot labels detected = ", detectedLabel)
 
-<<<<<<< HEAD
-            #filter the snapshot with labels
-            filter_labels(labels, labelFilter)
-=======
             # filter the snapshot with labels
             labelList = ['Vehicle', 'Vehicle registration plate', 'Car']
             filter_labels(detectedLabel, labelList)
->>>>>>> 9802c277f3f88f44099ba2fdf8caac9540fbd324
 
             # if car plate is not detected, send snapshot url to webex for manual check.
             # to minimize overhead, notification only include car/vehicle-related label
